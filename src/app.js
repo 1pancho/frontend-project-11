e14e3c52435d6fc0fb5e3bdef2026becf0215984
Timerlan Medov <medovcloud@me.com>
2023-09-12 20:07:05 +0300

    second step

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ export default () => {
     };
 
     const elements = {
+        form: document.querySelector('.rss-form'),
         input: document.getElementById('url-input'),
         submit: document.querySelector('button[type="submit"]'),
         feedback: document.querySelector('.feedback'),
@@ -35,7 +36,15 @@ export default () => {
 
     const watchedState = watch(state, element);
 
-    
+    elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const url = formData.get('url');
+        const alreadyAddedLinks = state.feeds.map((feed) => {
+            
+        })
+        const validatedUrl = validateUrl(url, feeds);
+    })
 
 }
 
